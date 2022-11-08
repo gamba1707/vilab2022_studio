6936333e72f979da1b0f16a221abc94d04ed34e8
SougoOmata <sougo.gamba.20001204@gmail.com>
2022-11-08 18:11:25 +0900

    20221108

---
+++ app/src/main/java/com/example/omata/vilab_studio/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.omata.vilab_studio;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 端末にインストール済のアプリケーション一覧情報を取得
+        final PackageManager pm = getPackageManager();
+        final int flags = PackageManager.GET_UNINSTALLED_PACKAGES | PackageManager.GET_DISABLED_COMPONENTS;
+        final List<ApplicationInfo> installedAppList = pm.getInstalledApplications(0);
+
+        // リストに一覧データを格納する
+        final List<AppData> dataList = new ArrayList<AppData>();
+        for (ApplicationInfo app : installedAppList) {
+            if (!((app.flags & ApplicationInfo.FLAG_SYSTEM) == ApplicationInfo.FLAG_SYSTEM)){
+                AppData data = new AppData();
+                data.label = app.loadLabel(pm).toString();
+                data.icon = app.loadIcon(pm);
+                data.pname = app.packageName;
+                dataList.add(data);
+            }
+
+        }
+
+        // リストビューにアプリケーションの一覧を表示する
+        final ListView listView = new ListView(this);
+        listView.setAdapter(new AppListAdapter(this, dataList));
+        //クリック処理
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ApplicationInfo item = installedAppList.get(position);
+                PackageManager pManager = getPackageManager();
+                Intent intent = pManager.getLaunchIntentForPackage(item.packageName);
+                startActivity(intent);
+            }
+        });
+        setContentView(listView);
+    }
+
+    // アプリケーションデータ格納クラス
+    private static class AppData {
+        String label;
+        Drawable icon;
+        String pname;
+    }
+
+    // アプリケーションのラベルとアイコンを表示するためのアダプタークラス
+    private static class AppListAdapter extends ArrayAdapter<AppData> {
+
+        private final LayoutInflater mInflater;
+
+        public AppListAdapter(Context context, List<AppData> dataList) {
+            super(context, R.layout.activity_main);
+            mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            addAll(dataList);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ViewHolder holder = new ViewHolder();
+
+            if (convertView == null) {
+                convertView = mInflater.inflate(R.layout.activity_main, parent, false);
+                holder.textLabel = (TextView) convertView.findViewById(R.id.label);
+                holder.imageIcon = (ImageView) convertView.findViewById(R.id.icon);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            // 表示データを取得
+            final AppData data = getItem(position);
+            // ラベルとアイコンをリストビューに設定
+            holder.textLabel.setText(data.label);
+            holder.imageIcon.setImageDrawable(data.icon);
+
+            return convertView;
+        }
+    }
+
+    // ビューホルダー
+    private static class ViewHolder {
+        TextView textLabel;
+        ImageView imageIcon;
+    }
+}
